be118374edc76dd52b1c660c9b62fc88099b77a7
Chen Kai <281165273grape@gmail.com>
2025-03-21 17:58:43 +0800

    fix file name error

--- src/root.zig
+++ src/root.zig
@@ -4,7 +4,7 @@ const testing = std.testing;
 pub const conn = @import("conn.zig");
 pub const frame = @import("frame.zig");
 // pub const stream = @import("stream.zig");
-pub const Config = @import("config.zig");
+pub const Config = @import("Config.zig");
 pub const session = @import("session.zig");
 
 test {
